edee9cb2bc2492c8a4892d914886bfea7d5f3fbd
koddaks <koss199710@gmail.com>
2023-10-16 13:13:12 +0300

    make live-example section without slider

--- source/js/slider-before-after.js
+++ source/js/slider-before-after.js
@@ -1,21 +1,21 @@
-let dragging = false;
+// let dragging = false;
 
-document.onmousedown = () => (dragging = true);
-document.onmouseup = () => (dragging = false);
+// document.onmousedown = () => (dragging = true);
+// document.onmouseup = () => (dragging = false);
 
-console.log(dragging);
+// console.log(dragging);
 
-document.onmousemove = (e) => {
-  const element = document.querySelector(".before-after"); // Замените '#your-element' на селектор вашего элемента
-  const isMouseInsideElement = element.contains(e.target);
+// document.onmousemove = (e) => {
+//   const element = document.querySelector(".before-after"); // Замените '#your-element' на селектор вашего элемента
+//   const isMouseInsideElement = element.contains(e.target);
 
-  console.log(isMouseInsideElement);
+//   console.log(isMouseInsideElement);
 
-  // если тянем, и курсор в районе картинки
-  if (dragging && isMouseInsideElement) {
-    let x = e.clientX;
+//   // если тянем, и курсор в районе картинки
+//   if (dragging && isMouseInsideElement) {
+//     let x = e.clientX;
 
-    document.querySelector(".pad").style.left = x + "px";
-    document.querySelector(".after").style.width = x + "px";
-  }
-};
+//     document.querySelector(".pad").style.left = x + "px";
+//     document.querySelector(".after").style.width = x + "px";
+//   }
+// };
