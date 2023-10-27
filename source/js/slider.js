5aa6c41e1a24571ebd4018ff10e7b5f4daf5bdb8
koddaks <koss199710@gmail.com>
2023-10-27 14:12:30 +0300

    make fix if view width change

--- source/js/slider.js
+++ source/js/slider.js
@@ -2,17 +2,25 @@ const slider = document.getElementById("slider");
 const imageBefore = document.querySelector(".slider__image-before-wrapper");
 const imageAfter = document.querySelector(".slider__image-after-wrapper");
 
-const isMobile = window.innerWidth <= 767; // Проверка на мобильное разрешение
+function updateSlider() {
+  const isMobile = window.innerWidth <= 767;
 
-if (isMobile) {
-  slider.step = "100"; // Установка шага 100% на мобильных устройствах
-  slider.value = "0"; // Установка шага 100% на мобильных устройствах
+  if (isMobile) {
+    slider.step = "100";
+    slider.value = "0";
 
-  imageBefore.style.clipPath = `inset(0% 0% 0% 0%)`;
-  imageAfter.style.clipPath = `inset(0% 0% 0% 100%)`;
-} else {
-  slider.step = "1"; // Установка стандартного шага на остальных устройствах
+    imageBefore.style.clipPath = `inset(0% 0% 0% 0%)`;
+    imageAfter.style.clipPath = `inset(0% 0% 0% 100%)`;
+  } else {
+    slider.step = "1";
+  }
 }
+
+
+updateSlider();
+
+// resize для обновления настроек при изменении размера окна
+window.addEventListener("resize", updateSlider);
 
 slider.addEventListener("input", () => {
   const value = slider.value;
