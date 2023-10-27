f4f07241a8c7cec58b595ed69e25bc8c7e2f9bab
koddaks <koss199710@gmail.com>
2023-10-27 14:07:52 +0300

    make slider before/after

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,9 +1,21 @@
-const slider = document.getElementById('slider');
-const imageBefore = document.querySelector('.image-before');
-const imageAfter = document.querySelector('.image-after');
+const slider = document.getElementById("slider");
+const imageBefore = document.querySelector(".slider__image-before-wrapper");
+const imageAfter = document.querySelector(".slider__image-after-wrapper");
 
-slider.addEventListener('input', () => {
+const isMobile = window.innerWidth <= 767; // Проверка на мобильное разрешение
+
+if (isMobile) {
+  slider.step = "100"; // Установка шага 100% на мобильных устройствах
+  slider.value = "0"; // Установка шага 100% на мобильных устройствах
+
+  imageBefore.style.clipPath = `inset(0% 0% 0% 0%)`;
+  imageAfter.style.clipPath = `inset(0% 0% 0% 100%)`;
+} else {
+  slider.step = "1"; // Установка стандартного шага на остальных устройствах
+}
+
+slider.addEventListener("input", () => {
   const value = slider.value;
-  imageBefore.style.clipPath = `inset(0% ${100 - value}% 0% 0%)`;
-  imageAfter.style.clipPath = `inset(0% 0% 0% ${value}%)`;
+  imageBefore.style.clipPath = `inset(0% ${value}% 0% 0%)`;
+  imageAfter.style.clipPath = `inset(0% 0% 0% ${100 - value}%)`;
 });
