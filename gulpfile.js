2a43ecaebbafadbabaccae668eac5532ba47bde4
koddaks <koss199710@gmail.com>
2023-11-12 15:32:33 +0200

    add scripts watcher

--- gulpfile.js
+++ gulpfile.js
@@ -131,6 +131,7 @@ const server = (done) => {
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series(styles));
   gulp.watch("source/*.html").on("change", html);
+  gulp.watch("source/js/*.js").on("change", gulp.series(scripts, browser.reload));
   gulp.watch("source/*.html").on("change", browser.reload);
 };
 
